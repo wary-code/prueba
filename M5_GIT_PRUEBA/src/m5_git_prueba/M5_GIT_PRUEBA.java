a5bbeab34d2d7665ef0c3fc5f1806e8a80949a2d
silvi <silvi@wary>
2023-02-27 19:24:05 +0100

    afeggit hello world!! doble

---
+++ M5_GIT_PRUEBA/src/m5_git_prueba/M5_GIT_PRUEBA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package m5_git_prueba;
+
+/**
+ *
+ * @author silvi
+ */
+public class M5_GIT_PRUEBA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
